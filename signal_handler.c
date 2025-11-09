98d9b94263f22815c56be0030b9d8dce15df36ff
Alexander Kruschka <kruschkaa@msoe.edu>
2025-11-09 11:48:01 -0600

    Modified code for exiting outside handler and added comments

--- signal_handler.c
+++ signal_handler.c
@@ -1,12 +1,15 @@
 /**
  * @file signal_handler.c
  * @brief Sets a signal handler for SIGINT, the handler prints a message and then quits
+ * Kill process with ctrl + c or typing kill -s SIGINT <PID> in terminal
  */
 
 /**
- * Modified by:
+ * Modified by: Alexander Kruschka
  * 
  * Brief summary of modifications:
+ * - Added volatile variable to determine whether to quit or not
+ * - Signal handler now changes the run to false instead of exiting program
  */
 
 
@@ -15,12 +18,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+volatile int run = 1;
+
 /**
  * @brief Signal handler for SIGINT - prints a message and exits
  */
 void handle_signal() {
     printf("Received a signal\n");
-    exit(1);
+    run = 0;
 }
 
 int main() {
@@ -28,11 +33,17 @@ int main() {
     // Register for the signal
     signal(SIGINT, handle_signal);
 
-    // Wait until a signal is received
+    // Wait until a signal is received, checking if program should quit
     while(1) {
         printf("Sleeping\n");
         sleep(1);
+        if (run == 0)
+        {
+            break;
+        }
     }
+
+    printf("Exiting outside of handler.\n");
 
     return 0;
 }
