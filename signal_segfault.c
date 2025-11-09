d161fe0eec0642b60372b0b8bf2bd48f7f20471f
Alexander Kruschka <kruschkaa@msoe.edu>
2025-11-09 13:13:20 -0600

    Added segfault handler

--- signal_segfault.c
+++ signal_segfault.c
@@ -4,15 +4,31 @@
  */
 
 /**
- * Modified by:
+ * Modified by: Alexander Kruschka
  * 
  * Brief summary of modifications:
+ * - Added a signal handler for SIGSEGV that prints a message
  */
 
-
+#include <signal.h>
 #include <stdio.h>
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * @brief Signal handler for SIGSEGV - prints a message and returns without performing anything else
+ */
+void handle_sigsegv()
+{
+    printf("Segmentation fault has occured!\n");
+    return;
+}
 
 int main (int argc, char* argv[]) {
+    
+    // Register for the signal
+    signal(SIGSEGV, handle_sigsegv);
+    
     // Declare a null pointer
     int* i = NULL;
 
