d0dd49d17a326540cbc8a4bb3826ebd63f2b1078
Alexander Kruschka <kruschkaa@msoe.edu>
2025-11-09 14:39:01 -0600

    Completed code and comments for sending and recieving signal between programs

--- recv_signal.c
+++ recv_signal.c
@@ -1,6 +1,41 @@
 /**
- * File: 
- * Modified by:
+ * File: recv_signal.c
+ * Modified by: Alexander Kruschka
  * 
- * Brief summary of program:
+ * Brief summary of program: Initializes and uses a sigusr1 handler that prints out an attached
+ *  sival_int value to the console in an infinite loop until forcefully exited.
  */
+
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+
+void handle_sigusr1(int signum, siginfo_t *info, void *context)
+{
+    if (signum == SIGUSR1)
+    {
+        printf("Recieved SIGUSR1 with sival_int value: %d\n", info->si_value.sival_int);
+        return;
+    }
+}
+
+int main()
+{
+    // Initialize sigaction struct
+    struct sigaction sigact;
+    sigact.sa_sigaction = handle_sigusr1;
+    sigact.sa_flags = SA_SIGINFO;
+
+    // Register signal handler for SIGUSR1
+    sigaction(SIGUSR1, &sigact, NULL);
+
+    // Enter indefinite loop for waiting
+    printf("Program running and waiting for SIGUSR1 signal.\n");
+    while (1)
+    {
+        sleep(1);
+    }
+
+    // Exit program
+    return 0;
+}
--- send_signal.c
+++ send_signal.c
@@ -1,6 +1,51 @@
 /**
- * File: 
- * Modified by:
+ * File: send_signal.c
+ * Modified by: Alexander Kruschka
  * 
- * Brief summary of program:
+ * Brief summary of program: Sends a random integer value using the sigqueue function
+ *  to another proram specified by the user entered PID in the command line with SIGUSR1.
  */
+
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+    // Initialize variables and unions
+    union sigval rand_val;
+    int pid_dest;
+
+    // Get a random value to be sent
+    srand(time(NULL));
+    rand_val.sival_int = rand();
+
+    // Error check for entered parameters
+    if (argc < 2)
+    {
+        printf("Error, enter a PID as a destination for the signal.\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    // Error check for non-integer input
+    if ((pid_dest = atoi(argv[1])) == -1)
+    {
+        printf("Error, please enter a valid integer.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Error check for sending signal and send if valid
+    if (sigqueue(pid_dest, SIGUSR1, rand_val) == -1)
+    {
+        printf("Error sending signal to entered PID, likely invalid PID.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // If all successful, print confirmation message with sent integer
+    printf("Sending %d with SIGUSR1 signal to PID: %d\n", rand_val.sival_int, pid_dest);
+
+    // Exit program
+    return 0;
+}
